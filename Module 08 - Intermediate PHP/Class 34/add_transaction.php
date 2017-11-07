86599d97e919e6e17ecf538a69d7eeecf5bfc4ba
Morgan Espindola <morgan.espindola@gmail.com>
2017-11-07 10:34:44 -0200

    Module 08 - Class 34
    Finished class 34 and module 08

--- Module 08 - Intermediate PHP/Class 34/add_transaction.php
+++ Module 08 - Intermediate PHP/Class 34/add_transaction.php
@@ -1,4 +1,42 @@
-<?php session_start() ?>
+<?php 
+
+session_start();
+require'config.php';
+
+if(isset($_POST['type']) && isset($_POST['value']) && !empty($_POST['value'])) {
+
+	$type = addslashes($_POST['type']);
+	$value = addslashes(str_replace(",",".",$_POST['value']));
+	$value = floatval($value);
+
+	$sql = $pdo->prepare("INSERT INTO historical (id_account, type, value, operation_date) VALUES (:id_account, :type, :value, NOW())");
+	$sql->bindValue(":id_account", $_SESSION['bank']);
+	$sql->bindValue(":type", $type);
+	$sql->bindValue(":value", $value);
+
+	$sql->execute();	
+
+	if($type == '0') {
+		// Deposit
+		$sql = $pdo->prepare("UPDATE accounts SET balance = balance + :value WHERE id = :id;");
+		$sql->bindValue(":value", $value);
+		$sql->bindValue(":id", $_SESSION['bank']);
+		$sql->execute();
+	} else {
+		// Withdrawal
+		$sql = $pdo->prepare("UPDATE accounts SET balance = balance - :value WHERE id = :id;");
+		$sql->bindValue(":value", $value);
+		$sql->bindValue(":id", $_SESSION['bank']);
+		$sql->execute();
+	}
+
+	header("Location: index.php");
+	exit;
+
+}
+
+
+ ?>
 
 <!DOCTYPE html>
 <html>
@@ -8,16 +46,16 @@
 </head>
 <body>
 	<form method="POST">
-		Agency: <br>
-		<input type="number" name="agency"><br><br>
+		Transaction Type: <br>
+		<select name="type">
+			<option value="0">Deposit</option>
+			<option value="1">Withdrawal</option>
+		</select><br><br>
 
-		Account: <br>
-		<input type="number" name="account"><br><br>
+		Value: <br>
+		<input type="text" name="value" pattern="[0-9.,]{1,}"><br><br>
 
-		Password: <br>
-		<input type="password" name="password"><br><br>
-
-		<input type="submit" value="Login"><br><br>
+		<input type="submit" value="Add">
 	</form>
 </body>
 </html> 
