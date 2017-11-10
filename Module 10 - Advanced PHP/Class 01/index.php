91f3746147e5a3d385ca64707fdec34bab463a08
Morgan Espindola <morgan.espindola@gmail.com>
2017-11-10 15:26:22 -0200

    Module 10 - Classes 01 to 02

--- Module 10 - Advanced PHP/Class 01/index.php
+++ Module 10 - Advanced PHP/Class 01/index.php
@@ -1,9 +1,26 @@
+<?php
+
+// 1 - first we need to change the file:
+// 	...\wamp64\bin\apache\apache2.4.23\conf\httpd.conf
+// 2 - find the line with "mod_rewrite"
+// 	uncomment the line (remove the "#"), it'll be like that: LoadModule rewrite_module modules/mod_rewrite.so
+// 3 - restart the apache
+// 4 - create a file called only ".htaccess"
+// 5 - insert into the file: 
+// 	RewriteEngine On // It'll enable the rewrite on our project
+// 	RewriteCond %{REQUEST_FILENAME} !-f // It'll ignore all access to real files on our project by url address
+// 	RewriteCond %{REQUEST_FILENAME} !-d // It'll ignore all access to real directories on our project by url address
+// 	RewriteRule ^(.*)$ /index.php?q=$1 [L] // create the rule to redirect the access
+
+?>
+
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Class 01 - Mod_rewrite</title>
-</head>
-<body>
+	<head>
+		<title>Class 01 - Mod_rewrite</title>
+	</head>
+	<body>
+		<?php print_r($_GET); ?>
 
-</body>
+	</body>
 </html>
---
+++ Module 10 - Advanced PHP/Class 02/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Class 02 - GD Library</title>
+	</head>
+	<body>
+		GD Library is used to work with imagens
+	</body>
+</html>
