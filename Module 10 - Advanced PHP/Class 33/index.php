4147927e7fb2a4c6c749a4ff9ecd6ccb1549fe13
Morgan Espindola <morgan.espindola@gmail.com>
2017-11-20 10:22:27 -0200

    Module 10 - Class 33

---
+++ Module 10 - Advanced PHP/Class 33/index.php
@@ -0,0 +1,40 @@
+<?php 
+
+/*
+* Class for test
+*
+* This class will get a x value and sum with an y value
+*
+* @package InventoryControl
+* @author Morgan <m.spdl@live.com>
+*/
+class Test {
+
+	private $name;
+
+	/*
+	* Function to sum
+	*
+	* This function will get a x value and sum with an y value
+	*
+	* @param $x int or float - is the first value to sum
+	* @param $y int or float - is the second value to sum
+	* @param $z int or float - is the third value to sum
+	* @return int or float
+	*/
+	public function sum($x, $y, $z) {
+		return ($x + $y + $z);
+	}
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Class 33 - Documenting your code</title>
+	</head>
+	<body>
+
+	</body>
+</html>
