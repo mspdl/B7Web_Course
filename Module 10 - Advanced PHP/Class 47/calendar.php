cba91374714d0f3b2f43e09d6a73dbd52849cddf
Morgan Espindola <morgan.espindola@gmail.com>
2017-11-28 20:31:57 -0200

    Module 10 - Class 47
    finished now

--- Module 10 - Advanced PHP/Class 47/calendar.php
+++ Module 10 - Advanced PHP/Class 47/calendar.php
@@ -1,11 +1,11 @@
-<?php 
-
+<?php /*
 echo "First day of the month: ".$first_day_month."<br>";
 echo "Month's days quantity: ".$total_days."<br>";
 echo "Month's week quantity: ".$weeks."<br>";
 echo "First day of the first week: ".$start_date."<br>";
 echo "Last day of the last week: ".$last_date."<br>";
-?>
+*/ ?>
+
 
 <table border="1" width="100%">
 	<tr>
@@ -21,12 +21,17 @@ echo "Last day of the last week: ".$last_date."<br>";
 		<tr>
 			<?php for($i=0;$i<7;$i++) : ?>
 				<?php 
-				$w = date('Y-m-d', strtotime(($i+($l*7)). 'days', strtotime($start_date))); ?>
-				<td><?php 
-				echo $w."<br><br>";
+				$t = strtotime(($i+($l*7)). 'days', strtotime($start_date));
+				$w = date('Y-m-d', $t); ?>
+				<td><?php
+				echo "<strong>".date('d/m',$t)."</strong><br/><br/>";
+				$w = strtotime($w);
 				foreach ($list as $item) {
-					$bd_start = $item['start_date']; //bd = booking data
-					$bd_end = $item['start_end'];
+					$bd_start = strtotime($item['start_date']); //bd = booking data
+					$bd_end = strtotime($item['end_date']); //bd = booking data
+					if ($w >= $bd_start && $w <= $bd_end) {
+						echo $item['id_car'].": ".$item['person']."<br/>";
+					}
 				}
 				?></td>
 			<?php endfor; ?>
--- Module 10 - Advanced PHP/Class 47/index.php
+++ Module 10 - Advanced PHP/Class 47/index.php
@@ -34,8 +34,6 @@ $reserves = new Booking($pdo);
 
 		?>
 
-
-		<hr>
 		<?php  
 		$date = $year."-".$month;
 		$first_day_month = date('w', strtotime($date));
@@ -47,13 +45,15 @@ $reserves = new Booking($pdo);
 
 		$list = $reserves->getReserves($start_date, $last_date); 
 
-		/*foreach ($list as $item) {
+		/*
+		foreach ($list as $item) {
 			$start_br_date = date('d/m/Y', strtotime($item['start_date']));
 			$end_br_date = date('d/m/Y', strtotime($item['end_date']));
 			echo $item['person'].' booked the car '.$item['id_car'].' between '.$start_br_date. ' and '.$end_br_date.'<br>';
-		}*/?>
+		}
+		*/?>
 
-		<hr>
+		<br/>
 		<?php require 'calendar.php' ?>
 		
 <?php include 'footer.html' ?>
