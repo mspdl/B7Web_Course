fa297326d2b4ecc8b1ebe17b21df2e223e82ef93
Morgan Espindola <morgan.espindola@gmail.com>
2017-12-01 21:13:54 -0200

    Module 08 - Class 31

--- Module 08 - Intermediate PHP/Class 31/index.php
+++ Module 08 - Intermediate PHP/Class 31/index.php
@@ -1,31 +1,15 @@
-<?php
+<?php 
 
-	session_start();
-	require'config.php';
 
-	if (empty($_SESSION['logged'])) {
-		header("Location: login.php");
-	}
+ ?>
 
-	$email = '';
-	$sql = "SELECT email,code FROM users WHERE id = '".addslashes($_SESSION['logged'])."'";
-	$sql = $pdo->query($sql);
-	if($sql->rowCount() > 0) {
-		$info = $sql->fetch();
-		$email = $info['email'];
-		$code = $info['code'];
-	}
 
-?>
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Class 31 - Project: Register by Invite</title>
-		<link rel="stylesheet" type="text/css" href="../style.css">
-	</head>
-	<body>
-		<h1>Internal System Area</h1>
-		<p>User: <?php echo $email; ?> - <a href="logout.php">Logout</a></p>
-		<p><a href="http://localhost/B7Web_Course/Module%2008%20-%20Intermediate%20PHP/Class%2031/register.php?code=<?php echo $code; ?>">Link for New Invitations</p></a>
-	</body>
-</html> 
+ <!DOCTYPE html>
+ <html>
+	 <head>
+	 	<title>Project: Multi-Level Marketing System (1/4)</title>
+	 </head>
+	 <body>
+	 
+	 </body>
+ </html>
--- Module 08 - Intermediate PHP/Class 31/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-	session_start();
-	unset($_SESSION['logged']);
-	header("Location: index.php");
-	exit; 
