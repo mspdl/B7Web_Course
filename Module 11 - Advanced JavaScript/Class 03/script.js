7bcb606541cd437706d5b6b9814fcbcfd723562c
DESKTOP-03O8QEC\Morgan <morgan.espindola@gmail.com>
2017-12-11 21:43:25 -0200

    Module 11 - Class 03

---
+++ Module 11 - Advanced JavaScript/Class 03/script.js
@@ -0,0 +1,13 @@
+function Animal() {
+	this.race = "Dog";
+	this.name = "Meggie";
+	this.age = "2";
+	this.weight = "5,8";
+}
+
+var meggie = new Animal();
+var lulu = new Animal();
+lulu.race = "Cat";
+lulu.name = "Lulu";
+lulu.age = "1";
+lulu.weight = "2,5";
