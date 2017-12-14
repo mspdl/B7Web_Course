e615c2f1020a85a769be94be4d8ccadadcaa0966
DESKTOP-03O8QEC\Morgan <morgan.espindola@gmail.com>
2017-12-13 23:00:03 -0200

    Module 12 - Class 06

---
+++ Module 12 - jQuery/Class 06/script.js
@@ -0,0 +1,11 @@
+// $("button") = to select all tags with "button" type
+// $("#btn1") = to select the tag with id="btn1"
+// $(".btn") = to select all tag with class="btn"
+// $(".list1 .btn") // to select a tag class inside another tag class
+// selecting a element is equal to select in css 
+
+// all commands above retorns an array, so, if I want to know if exists we use:
+// if($('.btn').length > 0) {}
+
+// I can attribute the tag element returns to a variable, like this:
+// var test = $('#btn1');
