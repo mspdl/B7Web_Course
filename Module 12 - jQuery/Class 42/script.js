1e01c9645ee4bf7e9576cf471d9c555fa83785bf
Morgan Espindola <morgan.espindola@gmail.com>
2017-12-28 17:53:58 -0200

    Module 12 - Class 42

--- Module 12 - jQuery/Class 42/script.js
+++ Module 12 - jQuery/Class 42/script.js
@@ -1,7 +1,13 @@
 $(function() {
 	$('button').on('click', function() {
-
-		$('.div').load('test.html');
-
+		/* 
+		$('.div').load('test.html'); 
+		$.get('test.html', function(t){ // function when get ends
+			$('.div').html(t);
+		});
+		*/
+		$.post('test.html', function(t){ // function when get ends
+			$('.div').html(t);
+		});
 	});
 });
