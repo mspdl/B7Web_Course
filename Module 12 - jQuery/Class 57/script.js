71ffbc3ce625a7dc9dd230567e5cdb08db5ecd28
Morgan Espindola <morgan.espindola@gmail.com>
2018-01-05 18:07:10 -0200

    Module 12 - Class 57

---
+++ Module 12 - jQuery/Class 57/script.js
@@ -0,0 +1,13 @@
+$(function(){
+
+	$('a.gallery').on('click', function(){
+		var img = $(this).find('img').attr('src');
+		$('.divbox img').attr('src', img);
+		$('.bgbox, .divbox').fadeIn('fast');
+	});
+
+	$('.bgbox, .divbox button').on('click', function(){
+		$('.bgbox, .divbox').fadeOut('fast');
+	});
+
+});
