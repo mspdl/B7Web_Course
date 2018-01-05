4841183ce47526c1e33923a92f11ba736f1953cd
Morgan Espindola <morgan.espindola@gmail.com>
2018-01-05 17:46:58 -0200

    Module 12 - Class 56

---
+++ Module 12 - jQuery/Class 56/script.js
@@ -0,0 +1,20 @@
+$(function(){
+
+	$('#obj').on('mousedown', function(){
+		$(this).on('mousemove', function(e){
+			var x = e.originalEvent.pageX;
+			var y = e.originalEvent.pageY;
+			
+			var width = $(this).width();
+			var height = $(this).height();
+
+			$(this).css('left', (x - (width/2))+'px');
+			$(this).css('top', (y - (height/2))+'px');
+		});
+	});
+
+	$('#obj').on('mouseup', function(){
+		$(this).off('mousemove');
+	});
+
+});
