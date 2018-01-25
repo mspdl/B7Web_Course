8ba1ebc279c5c0400e28f2ab97f630960951b967
Morgan Espindola <morgan.espindola@gmail.com>
2018-01-25 14:11:15 -0200

    Module 14 - Class 02

---
+++ Module 14 - Project - Creating a Classfied/Class 02/pages/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Class 02 - Classifieds Site (2/11)</title>
+		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+		<script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
+		<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="assets/js/script.js"></script>
+	</head>
+	<body>
+
+		<nav class="navbar navbar-inverse">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<a href="./" class="navbar-brand">Classified</a>
+				</div>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="">Register</a></li>
+					<li><a href="">Login</a></li>
+				</ul>
+			</div>
+		</nav>
