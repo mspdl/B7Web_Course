a7c63731a0ee6d27faff58859242d454b918d541
Morgan Espindola <morgan.espindola@gmail.com>
2018-01-26 14:03:27 -0200

    Module 14 - Class 04
    unfinished

---
+++ Module 14 - Project - Creating a Classfied/Class 04/index.php
@@ -0,0 +1,18 @@
+<?php require 'pages/header.php'; ?>
+
+		<div class="container-fluid">
+			<div class="jumbotron">
+				<h2>We have 999 adverts today.</h2>
+				<p>And more than 99 users registered.</p>
+			</div>
+			<div class="row">
+				<div class="col-sm-3">
+					<h4>Advanced Search</h4>
+				</div>
+				<div class="col-sm-9">
+					<h4>Last adverts</h4>
+				</div>
+			</div>
+		</div>
+
+<?php require 'pages/footer.php'; ?>
